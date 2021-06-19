6c2e6a19bc023e2948a93ac7a812a88395932ed4
FernandoM <00076617@uca.edu.sv>
2021-06-18 21:52:50 -0600

    quitando conversiones sin usar

--- controladores/securityController.js
+++ controladores/securityController.js
@@ -9,18 +9,18 @@ async function handleXMl(req,res){
         return convertidor.convertFunctions.txtToXML(req.body.file);
     }
 
-    //txt to json
-    if(req.body.tipo == "txt" && req.body.convertir_a == "json"){
-        return convertidor.convertFunctions.TxtToJson(req.body.file);
-    }
-
     //xml to txt
     if(req.body.tipo == "xml" && req.body.convertir_a == "txt"){
         return convertidor.convertFunctions.xmlToTxt(req.body.file);
     }
 
-    //xml to json
-    if(req.body.tipo == "xml" && req.body.convertir_a == "json"){
+    //txt to json
+    if(req.body.tipo == "txt" && req.body.convertir_a == "json"){
+        return convertidor.convertFunctions.TxtToJson(req.body.file);
+    }
+
+    //json to txt
+    if(req.body.tipo == "json" && req.body.convertir_a == "txt"){
         return convertidor.convertFunctions.XmlToJson(req.body.file);
     }
 
--- utils/convertirTxtXML.js
+++ utils/convertirTxtXML.js
@@ -10,7 +10,13 @@ function xmlToTxt(file){
 }
 
 function JsonToTxt(file){
+    const myObj = { name: "John", age: 31, city: "New York" };
+    const myJSON = JSON.stringify(myObj);
+    localStorage.setItem("testJSON", myJSON);
 
+    // Retrieving data:
+    let text = localStorage.getItem("testJSON");
+    let obj = JSON.parse(text);
 }
 
 function TxtToJson(file){
