0006b76bf80e566a336aefcf81a7dc41fc0e57b3
GerryMG2 <00032515@uca.edu.sv>
2021-06-18 23:45:54 -0600

    merged

--- controladores/securityController.js
+++ controladores/securityController.js
@@ -5,7 +5,6 @@ const convertidor = require("../utils/convertirTxtXML");
 async function handleXMl(req,res){
     
     //txt to xml
-<<<<<<< HEAD
     try {
         if(req.body.tipo == "txt" && req.body.convertir_a == "xml"){
             res.status(200).json({result: convertidor.convertFunctions.txtToXML(req.body.file), 
@@ -35,28 +34,6 @@ async function handleXMl(req,res){
         res.status(404).json({result: 'Error'})
     }
    
-=======
-    if(req.body.tipo == "txt" && req.body.convertir_a == "xml"){
-        return convertidor.convertFunctions.txtToXML(req.body.file);
-    }
-
-    //xml to txt
-    if(req.body.tipo == "xml" && req.body.convertir_a == "txt"){
-        return convertidor.convertFunctions.xmlToTxt(req.body.file);
-    }
-
-    //txt to json
-    if(req.body.tipo == "txt" && req.body.convertir_a == "json"){
-        return convertidor.convertFunctions.TxtToJson(req.body.file);
-    }
-
-    //json to txt
-    if(req.body.tipo == "json" && req.body.convertir_a == "txt"){
-        return convertidor.convertFunctions.XmlToJson(req.body.file);
-    }
-
-
->>>>>>> 2593c0dc0b6a56e8961bcc0520748f9d0e0abb4f
 
 
 
