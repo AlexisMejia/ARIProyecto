8d223b304600c3f0de44a5ff11546855c6c00d9e
GerryMG2 <00032515@uca.edu.sv>
2021-06-18 21:27:50 -0600

    4 casos de  converssion controller

--- controladores/securityController.js
+++ controladores/securityController.js
@@ -1,19 +1,29 @@
 
-
+const convertidor = require("../utils/convertirTxtXML");
 
 
 async function handleXMl(req,res){
     
     //txt to xml
     if(req.body.tipo == "txt" && req.body.convertir_a == "xml"){
-
+        return convertidor.convertFunctions.txtToXML(req.body.file);
     }
 
-    
-
     //txt to json
+    if(req.body.tipo == "txt" && req.body.convertir_a == "json"){
+        return convertidor.convertFunctions.TxtToJson(req.body.file);
+    }
+
     //xml to txt
+    if(req.body.tipo == "xml" && req.body.convertir_a == "txt"){
+        return convertidor.convertFunctions.xmlToTxt(req.body.file);
+    }
+
     //xml to json
+    if(req.body.tipo == "xml" && req.body.convertir_a == "json"){
+        return convertidor.convertFunctions.XmlToJson(req.body.file);
+    }
+
     //json to txt
     //json to xml
 
