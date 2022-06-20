4b29f7c78ad6486999ca6f866cf926bd260015be
AlexisMejia <00090718@uca.edu.sv>
2022-06-19 19:47:24 -0600

    Layout: display form

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,3 @@
-
-
-
 var btn = document.getElementById("btn_submit");
 var file = document.getElementById("formFile");
 var option = document.getElementById("formSelect");
@@ -14,27 +11,27 @@ var resultadof = document.getElementById("resultf");
 btn.addEventListener("click", handleSubmit);
 
 
-function handleSubmit(e){
+function handleSubmit(e) {
     e.preventDefault();
     var fetchjson = {
-        
+
 
     };
-    if(file.value.split(".")[1] == 'txt' || 
-     file.value.split(".")[1] == 'xml' || file.value.split(".")[1] == 'json'){
-        
-        if(file.value.split(".")[1] != option.options[option.selectedIndex].text ){
+    if (file.value.split(".")[1] == 'txt' ||
+        file.value.split(".")[1] == 'xml' || file.value.split(".")[1] == 'json') {
+
+        if (file.value.split(".")[1] != option.options[option.selectedIndex].text) {
             console.log(clave.value.length);
-            if(clave.value.length == 16){
+            if (clave.value.length == 16) {
                 fetchjson.tipo = file.value.split(".")[1];
                 fetchjson.convertir_a = option.options[option.selectedIndex].text;
-                
+
                 fetchjson.clave = clave.value;
                 fetchjson.delimitador = delimitador.value;
                 var myFile = file.files[0];
                 var reader = new FileReader();
                 reader.readAsText(myFile);
-                reader.onload=function(){
+                reader.onload = function () {
                     var strfile = reader.result;
                     origen.value = reader.result;
                     fetchjson.file = strfile;
@@ -43,97 +40,97 @@ function handleSubmit(e){
                     var s = strfile;
                     var m;
                     let newstrfile = strfile;
-                    do{
+                    do {
                         m = re.exec(s);
-                        if(m){
+                        if (m) {
                             console.log(m);
-                            var aux = strfile.substr(m.index,16);
+                            var aux = strfile.substr(m.index, 16);
                             console.log(aux);
-                            var aux_cifrado = cifrado(aux,clave.value);
+                            var aux_cifrado = cifrado(aux, clave.value);
                             console.log(aux_cifrado);
-                            var descifrado = decifrado(aux_cifrado,clave.value);
+                            var descifrado = decifrado(aux_cifrado, clave.value);
                             console.log(descifrado);
                             newstrfile = newstrfile.replace(aux, (macth) => {
                                 return aux_cifrado;
                             })
                         }
-                    } while(m)
-                    
+                    } while (m)
+
                     fetchjson.file = newstrfile
-                    
+
                     request(fetchjson);
-                    
+
                 }
-            }else{
+            } else {
                 alert("la llave deben ser 16 numeros enteros sin espacios");
             }
 
-            
-            
-        }else{
+
+
+        } else {
             alert("valores iguales a convertir, ingrese un valor diferente");
         }
 
-    }else{
-        alert("El archivo no es del tipo correcto");
+    } else {
+        alert("Favor adjuntar la información solicitada para convertir los archivos");
     }
 
-    
+
 }
 
-function request(data){
+function request(data) {
     var url = '/';
-    
-    
+
+
     fetch(url, {
-      method: 'POST', // or 'PUT'
-      body: JSON.stringify(data), // data can be `string` or {object}!
-      headers:{
-        'Content-Type': 'application/json'
-      }
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
     }).then(res => res.json())
-    .catch(error => console.error('Error:'))
-    .then(response => {
+        .catch(error => console.error('Error:'))
+        .then(response => {
 
-        console.log(response);
-        var re = /[0-9]{16}/g;
-        var s = response.result;
-        var m;
-        let newstrfile = response.result;
-        do{
-            m = re.exec(s);
-            if(m){
-                console.log(m);
-                var aux = response.result.substr(m.index,16);
-                console.log(aux);
-                
-                var descifrado = decifrado(aux,clave.value);
-                console.log(descifrado);
-                newstrfile = newstrfile.replace(aux, (macth) => {
-                    return descifrado;
-                })
+            console.log(response);
+            var re = /[0-9]{16}/g;
+            var s = response.result;
+            var m;
+            let newstrfile = response.result;
+            do {
+                m = re.exec(s);
+                if (m) {
+                    console.log(m);
+                    var aux = response.result.substr(m.index, 16);
+                    console.log(aux);
+
+                    var descifrado = decifrado(aux, clave.value);
+                    console.log(descifrado);
+                    newstrfile = newstrfile.replace(aux, (macth) => {
+                        return descifrado;
+                    })
+                }
+            } while (m)
+
+            resultadof.value = newstrfile.toString();
+            if (response.tipo == "xml") {
+                dnl.classList.remove("d-none");
+                dnl.setAttribute('href', 'data:text/xml;charset=utf-8,' + encodeURIComponent(newstrfile))
+                dnl.setAttribute('download', "resultado.xml");
             }
-        } while(m)
 
-        resultadof.value = newstrfile.toString();
-        if(response.tipo == "xml"){
-            dnl.classList.remove("d-none");
-            dnl.setAttribute('href', 'data:text/xml;charset=utf-8,' + encodeURIComponent(newstrfile))
-            dnl.setAttribute('download', "resultado.xml");
-        }
+            if (response.tipo == "txt") {
+                dnl.classList.remove("d-none");
+                dnl.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(newstrfile))
+                dnl.setAttribute('download', "resultado.txt");
+            }
 
-        if(response.tipo == "txt"){
-            dnl.classList.remove("d-none");
-            dnl.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(newstrfile))
-            dnl.setAttribute('download', "resultado.txt");
-        }
+            if (response.tipo == "json") {
+                dnl.classList.remove("d-none");
+                dnl.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(newstrfile))
+                dnl.setAttribute('download', "resultado.json");
+            }
 
-        if(response.tipo == "json"){
-            dnl.classList.remove("d-none");
-            dnl.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(newstrfile))
-            dnl.setAttribute('download', "resultado.json");
-        }
-        
-    });
+        });
 }
 
